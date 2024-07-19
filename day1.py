3c2ced8c449799a2fd34c266953d72dd89f3edf8
Muhammad Ali Shafique <alikklcgg@gmail.com>
2024-07-19 15:00:30 +0500

    ali

--- day1.py
+++ day1.py
@@ -1,4 +1,1 @@
 print ("Hello Internees :)");
-print ( " I am Ali this is my 1st day at Internship ");
-print(" I will buy S-class Mercedez, G63, C63 AMG, Lc-300,Lx-600 , mercedez-Eqs ,Porsche In'shaaa;Allah ");
-print(" hello, this is my 2nd day");
--- day4.py
+++ day4.py
@@ -1,6 +1,3 @@
-print(" My name is Muhammad Ali Shafique. I am from Wah Cantt. I am currently studying BS-CS in PAF-IAST.My goal is to build a company that comprises of IT professionals and different fields");
-
-print(" ")
 print ("Hello Internees :)");
 print ( " I am Ali this is my 1st day at Internship ");
 print(" I will buy S-class Mercedez, G63, C63 AMG, Lc-300,Lx-600 , mercedez-Eqs ,Porsche In'shaaa;Allah ");
